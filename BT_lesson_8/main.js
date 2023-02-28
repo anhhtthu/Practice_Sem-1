b314a63bcd16fb1b00d40b1e750fd99cd1fe23e5
anhhtthu <thulha17@gmail.com>
2023-02-28 20:55:15 +0700

    initial commit

--- BT_lesson_8/main.js
+++ BT_lesson_8/main.js
@@ -1,18 +1,18 @@
 //homework 1
 function minuteCall(number) {
-  let soCuoc = 25000;
+  let soCuoc = 0;
   if (number >= 50) {
     soCuoc = 600 * number;
-    console.log("so tien gia dinh phai tra la: ", soCuoc);
+    console.log("so tien gia dinh phai tra la: ", soCuoc + 25000);
   } else if (number >= 150) {
     soCuoc = 400 * number;
-    console.log("so tien gia dinh phai tra la: ", soCuoc);
+    console.log("so tien gia dinh phai tra la: ", soCuoc + 25000);
   } else if (number >= 200) {
     soCuoc = 200 * number;
-    console.log("so tien gia dinh phai tra la: ", soCuoc);
+    console.log("so tien gia dinh phai tra la: ", soCuoc + 25000);
   }
 }
-minuteCall(200);
+minuteCall(60);
 
 //homework 2 Viết function Javascript ⇒ tính tổng các `số chẵn` từ 1 tới `n` _(Gợi ý sử dụng vòng lặp)
 function lastNum(number) {
