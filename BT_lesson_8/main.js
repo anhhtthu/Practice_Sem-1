f6184800e5dd552278afa009a0bf8bbf65698d3a
anhhtthu <thulha17@gmail.com>
2023-02-28 20:25:58 +0700

    initial commit

---
+++ BT_lesson_8/main.js
@@ -0,0 +1,27 @@
+//homework 1
+function minuteCall(number) {
+  let soCuoc = 25000;
+  if (number >= 50) {
+    soCuoc = 600 * number;
+    console.log("so tien gia dinh phai tra la: ", soCuoc);
+  } else if (number >= 150) {
+    soCuoc = 400 * number;
+    console.log("so tien gia dinh phai tra la: ", soCuoc);
+  } else if (number >= 200) {
+    soCuoc = 200 * number;
+    console.log("so tien gia dinh phai tra la: ", soCuoc);
+  }
+}
+minuteCall(200);
+
+//homework 2 Viết function Javascript ⇒ tính tổng các `số chẵn` từ 1 tới `n` _(Gợi ý sử dụng vòng lặp)
+function lastNum(number) {
+  let tongSoChan = 0;
+  for (m = 1; m < number; m++) {
+    if (m % 2 === 0) {
+      tongSoChan += m;
+      console.log("tong so chan la: ", tongSoChan);
+    }
+  }
+}
+lastNum(8);
